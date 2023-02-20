d8c43fc4940233d9903d92293f1b041185a838c9
Andrulkog <andygraglia@gmail.com>
2023-02-20 04:48:54 -0300

    preentrega3 v1.0.1 base

---
+++ AppCoder/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class CursoFormulario(forms.Form):
+    #Especificar los campos
+    curso = forms.CharField()
+    camada = forms.IntegerField()
+    
+class ProfesorFormulario(forms.Form):
+    nombre=forms.CharField(max_length=30)
+    apellido=forms.CharField(max_length=30)
+    email = forms.EmailField()
+    profesion = forms.CharField(max_length=30)
+
+class PruebaFormulario(forms.Form):
+    Cliente = forms.CharField(max_length = 200)
+    Campaña = forms.CharField(max_length = 200)
